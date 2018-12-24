a974c77c0204c93c605001edb4df1a88bd301941
zhugucai <zhugucai@meidaitech.com>
2018-12-24 19:58:48 +0800

    done loop test

---
+++ LoopSubdivisionSurface/test.cpp
@@ -0,0 +1,427 @@
+/* ========================================================================= *
+*                                                                           *
+*                               OpenMesh                                    *
+*           Copyright (c) 2001-2015, RWTH-Aachen University                 *
+*           Department of Computer Graphics and Multimedia                  *
+*                          All rights reserved.                             *
+*                            www.openmesh.org                               *
+*                                                                           *
+*---------------------------------------------------------------------------*
+* This file is part of OpenMesh.                                            *
+*---------------------------------------------------------------------------*
+*                                                                           *
+* Redistribution and use in source and binary forms, with or without        *
+* modification, are permitted provided that the following conditions        *
+* are met:                                                                  *
+*                                                                           *
+* 1. Redistributions of source code must retain the above copyright notice, *
+*    this list of conditions and the following disclaimer.                  *
+*                                                                           *
+* 2. Redistributions in binary form must reproduce the above copyright      *
+*    notice, this list of conditions and the following disclaimer in the    *
+*    documentation and/or other materials provided with the distribution.   *
+*                                                                           *
+* 3. Neither the name of the copyright holder nor the names of its          *
+*    contributors may be used to endorse or promote products derived from   *
+*    this software without specific prior written permission.               *
+*                                                                           *
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
+* OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
+*                                                                           *
+* ========================================================================= */
+/*===========================================================================*\
+*                                                                           *
+*   $Revision$                                                         *
+*   $Date$                   *
+*                                                                           *
+\*===========================================================================*/
+#include <iostream>
+// -------------------- OpenMesh
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/Traits.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/TriMeshT.hh>
+#include <set>
+// ----------------------------------------------------------------------------
+struct MyTraits : public OpenMesh::DefaultTraits
+{
+	// store new position of this vertex
+	VertexTraits
+	{
+	private:
+		Point  _newPosition; //endpoint new position
+		bool _isNew;
+	public:
+		VertexT() : _newPosition(Point(0.0f, 0.0f, 0.0f)), _isNew(true) { }
+		const Point& newPosition() const { return _newPosition; }
+		void set_newPosition(const Point& _p) { _newPosition = _p; }
+		const bool& isNew() const { return _isNew; }
+		void set_isNew(const bool& _b) { _isNew = _b; }
+		void update() { }
+	};
+
+	EdgeTraits
+	{
+	private:
+		Point  _newPosition; //midpoint new position
+		bool _isNew;
+	public:
+		EdgeT() : _newPosition(Point(0.0f, 0.0f, 0.0f)), _isNew(true) { }
+		const Point& newPosition() const { return _newPosition; }
+		const bool& isNew() const { return _isNew; }
+		void set_newPosition(const Point& _p) { _newPosition = _p; }
+		void set_isNew(const bool& _b) { _isNew = _b; }
+	};
+};
+
+typedef OpenMesh::DefaultTraits::Point Point;
+typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits>  MyMesh;
+typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits>  MyTriMesh;
+//typedef OpenMesh::TriMeshT<MyTraits> MyTriMesh2;
+// ----------------------------------------------------------------------------
+// Build a simple cube and write it to std::cout
+
+
+Point computeVertexNewPosition(MyTriMesh& m)
+{
+	return Point();
+}
+
+int main()
+{
+	MyTriMesh mesh;
+	// generate vertices
+	MyMesh::VertexHandle vhandle[8];
+	vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1, 1));
+	vhandle[1] = mesh.add_vertex(MyMesh::Point(1, -1, 1));
+	vhandle[2] = mesh.add_vertex(MyMesh::Point(1, 1, 1));
+	vhandle[3] = mesh.add_vertex(MyMesh::Point(-1, 1, 1));
+	vhandle[4] = mesh.add_vertex(MyMesh::Point(-1, -1, -1));
+	vhandle[5] = mesh.add_vertex(MyMesh::Point(1, -1, -1));
+	vhandle[6] = mesh.add_vertex(MyMesh::Point(1, 1, -1));
+	vhandle[7] = mesh.add_vertex(MyMesh::Point(-1, 1, -1));
+	// generate (quadrilateral) faces
+	std::vector<MyMesh::VertexHandle>  face_vhandles;
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[3]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[7]);
+	face_vhandles.push_back(vhandle[6]);
+	face_vhandles.push_back(vhandle[5]);
+	face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[4]);
+	face_vhandles.push_back(vhandle[5]);
+	mesh.add_face(face_vhandles);
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[5]);
+	face_vhandles.push_back(vhandle[6]);
+	mesh.add_face(face_vhandles);
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[3]);
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[6]);
+	face_vhandles.push_back(vhandle[7]);
+	mesh.add_face(face_vhandles);
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[3]);
+	face_vhandles.push_back(vhandle[7]);
+	face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+
+	try
+	{
+		if (!OpenMesh::IO::write_mesh(mesh, "output1.off"))
+		{
+			std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+	//MyTriMesh2 mymesh22;
+
+	//set the edges and vertices to be old ones;
+	std::cout << "there are " << mesh.n_edges() << " edges before split" << std::endl;
+	for (auto it = mesh.edges_begin(); it != mesh.edges_end(); ++it)
+	{
+		mesh.data(*it).set_isNew(false);
+		Point newPosition(0, 0, 0);
+		if (!mesh.is_boundary(*it))
+		{
+			//std::cout << "!is_boundary " << std::endl;
+			//std::cout << "1cal newposition " << newPosition[0] << " , " << newPosition[1] << " , " << newPosition[2] << std::endl;
+			auto he = mesh.halfedge_handle(it, 0);
+			auto he_twin = mesh.opposite_halfedge_handle(he);
+
+			auto v_connect1 = mesh.to_vertex_handle(he);
+			auto temp_pos = mesh.point(v_connect1);
+			std::cout << "v_connect1 newposition " << temp_pos[0] << " , " << temp_pos[1] << " , " << temp_pos[2] << std::endl;
+			auto v_connect2 = mesh.to_vertex_handle(he_twin);
+			temp_pos = mesh.point(v_connect2);
+			std::cout << "v_connect2 newposition " << temp_pos[0] << " , " << temp_pos[1] << " , " << temp_pos[2] << std::endl;
+			newPosition += 3.0 / 8.0 * (mesh.point(v_connect1) + mesh.point(v_connect2));
+			//std::cout << "2cal newposition " << newPosition[0] << " , " << newPosition[1] << " , " << newPosition[2] << std::endl;
+
+			auto he_far = mesh.next_halfedge_handle((he));
+			auto he_twin_far = mesh.next_halfedge_handle((he_twin));
+
+			auto v_far1 = mesh.to_vertex_handle(he_far);
+			temp_pos = mesh.point(v_far1);
+			std::cout << "v_far1 newposition " << temp_pos[0] << " , " << temp_pos[1] << " , " << temp_pos[2] << std::endl;
+			auto v_far2 = mesh.to_vertex_handle(he_twin_far);
+			temp_pos = mesh.point(v_far2);
+			std::cout << "v_far2 newposition " << temp_pos[0] << " , " << temp_pos[1] << " , " << temp_pos[2] << std::endl;
+			newPosition += 1.0 / 8.0 * (mesh.point(v_far1) + mesh.point(v_far2));
+			//std::cout << "3cal newposition " << newPosition[0] << " , " << newPosition[1] << " , " << newPosition[2] << std::endl;
+		}
+		else
+		{
+			std::cout << "is_boundary ";
+			auto he = mesh.halfedge_handle(it, 0);
+			auto he_twin = mesh.opposite_halfedge_handle(he);
+
+			auto v_connect1 = mesh.to_vertex_handle(he);
+			auto v_connect2 = mesh.to_vertex_handle(he_twin);
+			newPosition += 1.0 / 2.0 * (mesh.point(v_connect1) + mesh.point(v_connect2));
+		}
+		mesh.data(*it).set_newPosition(newPosition);
+		std::cout << "cal newposition " << newPosition[0] << " , " << newPosition[1] << " , " << newPosition[2] << std::endl;
+	}
+
+	for (auto it = mesh.vertices_begin(); it != mesh.vertices_end(); ++it)
+	{
+		mesh.data(*it).set_isNew(false);
+		Point newPosition;
+		if (!mesh.is_boundary(*it))
+		{
+			int degree = 0;
+			Point sum(0, 0, 0);
+			float beta;
+			for (auto vvit = mesh.vv_iter(*it); vvit.is_valid(); ++vvit)
+			{
+				Point p = mesh.point(*vvit);
+				sum += p;
+				degree++;
+			}
+			if (degree == 3)
+				beta = 3. / 16.;
+			else
+				beta = 3. / (8. * degree);
+			newPosition = (1. - degree * beta) * mesh.point(*it) + beta * sum;
+		}
+		else
+		{
+			Point sum(0, 0, 0);
+			int cnt = 0;
+			for (auto vvit = mesh.vv_iter(*it); vvit.is_valid(); ++vvit)
+			{
+				if (mesh.is_boundary(*vvit))
+				{
+					sum += mesh.point(*vvit);
+					cnt++;
+				}
+			}
+			assert(cnt == 2);
+			newPosition = 3.0 / 4.0 * mesh.point(*it) + 1.0 / 4 * sum;
+		}
+		mesh.data(*it).set_newPosition(newPosition);
+	}
+
+	//1st split the edge
+	auto edge_end = mesh.edges_end();
+	for (auto it = mesh.edges_begin(); it != edge_end; ++it)
+	{
+		if (!mesh.data(*it).isNew())
+		{
+			std::cout << "is a old edge, split it" << std::endl;
+			mesh.data(*it).set_isNew(false);
+			auto e = mesh.edge(*it);
+			auto he_handle = mesh.halfedge_handle(it, 0);
+			
+			auto to_v = mesh.to_vertex_handle(he_handle);
+			auto from_v = mesh.from_vertex_handle(he_handle);
+			auto to_p = mesh.point(to_v);
+			auto from_p = mesh.point(from_v);
+			auto mid_p = (to_p + from_p) / 2;
+			std::cout << " mid_point " << mid_p[0] << " , " << mid_p[1] << " , " << mid_p[2] << std::endl;
+			mid_p = mesh.data(*it).newPosition();
+			std::cout << " newposition " << mid_p[0] << " , " << mid_p[1] << " , " << mid_p[2] << std::endl;
+			auto new_v = mesh.split(it, mid_p);
+			mesh.set_point(new_v, mesh.data(*it).newPosition());
+			auto new_he1 = mesh.find_halfedge(new_v, from_v);
+			auto new_he2 = mesh.find_halfedge(new_v, to_v);
+			auto new_e1 = mesh.edge_handle(new_he1);
+			auto new_e2 = mesh.edge_handle(new_he2);
+
+			mesh.data(new_e1).set_isNew(false);
+			mesh.data(new_e2).set_isNew(false);
+		}
+	}
+
+	try
+	{
+		if (!OpenMesh::IO::write_mesh(mesh, "output1_split.off"))
+		{
+			std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+
+	//2nd flip the edge
+	int cnt_ = 0;
+	for (auto it = mesh.edges_begin(); it != mesh.edges_end(); ++it)
+	{
+		if (mesh.data(*it).isNew())
+		{
+			auto he_handle = mesh.halfedge_handle(it, 0);
+			auto to_v = mesh.to_vertex_handle(he_handle);
+			auto from_v = mesh.from_vertex_handle(he_handle);
+			if (mesh.is_flip_ok(it) && ((mesh.data(to_v).isNew() && !mesh.data(from_v).isNew()) || (!mesh.data(to_v).isNew() && mesh.data(from_v).isNew())))
+			{
+				std::cout << "flip " << ++cnt_ << " edge" << std::endl;
+				mesh.flip(it);
+				//break;
+			}
+		}
+	}
+
+	for (auto it = mesh.vertices_begin(); it != mesh.vertices_end(); ++it)
+	{
+		if (!mesh.data(*it).isNew())
+			mesh.set_point(it, mesh.data(*it).newPosition());
+	}
+
+	//mesh.
+	// write mesh to output.obj
+	try
+	{
+		if (!OpenMesh::IO::write_mesh(mesh, "output1_subdivide.off"))
+		{
+			std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+	return 0;
+	MyTriMesh mesh2;
+	//TriMesh mesh3;
+	try
+	{
+		if (!OpenMesh::IO::read_mesh(mesh2, "test.ply"))
+		{
+			std::cerr << "Cannot read mesh2 from file 'test.ply'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+	//std::set<OpenMesh::PolyConnectivity::EdgeIter> isRead;
+	int cnt = 0;
+	for (auto it = mesh2.edges_begin(); it != mesh2.edges_end(); ++it)
+	{
+		//if (isRead.find(it) != isRead.end())
+		{
+			mesh2.data(*it).set_isNew(false);
+			std::cout << "set " << ++cnt << " edge as an old edge" << std::endl;
+		}
+	}
+	cnt = 0;
+	for (auto it = mesh2.vertices_begin(); it != mesh2.vertices_end(); ++it)
+	{
+		//if (isRead.find(it) != isRead.end())
+		{
+			mesh2.data(*it).set_isNew(false);
+			std::cout << "set " << ++cnt << " vertex as an old edge" << std::endl;
+		}
+	}
+
+	std::cout << "there are " << mesh2.n_edges() << " edges before split" << std::endl;
+	std::cout << "there are " << mesh2.n_faces() << " faces before split" << std::endl;
+	std::cout << "there are " << mesh2.n_vertices() << " vertices before split" << std::endl;
+	std::cout << "there are " << cnt << " edge before split" << std::endl;
+	//return 0;
+	for (auto it = mesh2.edges_begin(); it != mesh2.edges_end(); ++it)
+	{
+		if (!mesh2.data(*it).isNew())
+		{
+			std::cout << "is a old edge, split it" << std::endl;
+			auto e = mesh2.edge(*it);
+			auto he_handle = mesh2.halfedge_handle(it, 0);
+			auto to_v = mesh2.to_vertex_handle(he_handle);
+			auto from_v = mesh2.from_vertex_handle(he_handle);
+			auto to_p = mesh2.point(to_v);
+			auto from_p = mesh2.point(from_v);
+			auto mid_p = (to_p + from_p) / 2;
+			mesh2.split(it, mid_p);
+			std::cout << "there are " << mesh2.n_edges() << " edge after split" << std::endl;
+			//break;
+		}
+	}
+	std::cout << "there are " << mesh2.n_edges() << " edge after split" << std::endl;
+	cnt = 0;
+	for (auto it = mesh2.edges_begin(); it != mesh2.edges_end(); ++it)
+	{
+		if (mesh2.data(*it).isNew())
+		{
+			auto he_handle = mesh2.halfedge_handle(it, 0);
+			auto to_v = mesh2.to_vertex_handle(he_handle);
+			auto from_v = mesh2.from_vertex_handle(he_handle);
+			if (mesh2.is_flip_ok(it) && ((mesh2.data(to_v).isNew() && !mesh2.data(from_v).isNew()) || (!mesh2.data(to_v).isNew() && mesh2.data(from_v).isNew())))
+			{
+				std::cout << "flip " << cnt++ << " edge" << std::endl;
+				mesh2.flip(it);
+			}
+		}
+	}
+	try
+	{
+		if (!OpenMesh::IO::write_mesh(mesh2, "output2_split.obj"))
+		{
+			std::cerr << "Cannot write mesh2 to file 'output.obj'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
--- src/HalfMesh/HalfMesh.h
+++ src/HalfMesh/HalfMesh.h
@@ -2,6 +2,7 @@
 #include <list>
 #include <glm/vec3.hpp>
 
+
 using namespace std;
 namespace B_rep
 {
@@ -34,6 +35,8 @@ namespace B_rep
 		Edge() {};
 		~Edge() {};
 
+		Vector3f newPosition; // the position of midpoint when doing loop division
+		bool isNew;
 		HalfEdgeIter & half_edge() { return _half_edge; };
 	private:
 		HalfEdgeIter _half_edge;
@@ -45,24 +48,28 @@ namespace B_rep
 		HalfEdge() {};
 		~HalfEdge() {};
 
-		VertexIter & vertex() { return _vertex; }
-		HalfEdgeIter &twin() { return _twin; }
-		HalfEdgeIter &next() { return _next; }
-		EdgeIter &edge() { return _edge; }
+		VertexIter& vertex() { return _vertex; }
+		HalfEdgeIter& twin() { return _twin; }
+		HalfEdgeIter& next() { return _next; }
+		EdgeIter& edge() { return _edge; }
+		FaceIter& face() { return _face; }
 	private:
 		HalfEdgeIter _twin;
 		HalfEdgeIter _next;
 		VertexIter _vertex;
 		EdgeIter _edge;
+		FaceIter _face;
 	};
 
 	class Vertex
 	{
 	public:
 		Vertex() {};
-		Vertex(Vector3f _pos) : pos(_pos){};
+		Vertex(Vector3f _pos) : position(_pos){};
 		~Vertex() {};
-		Vector3f pos;
+
+		Vector3f newPosition; // for loop division update.
+		Vector3f position;
 		HalfEdgeIter & half_edge() { return _half_edge; };
 	private:
 		HalfEdgeIter _half_edge;
@@ -80,6 +87,8 @@ namespace B_rep
 		EdgeIter     newEdge() { return      _edges.insert(_edges.end(), Edge()); }
 		FaceIter     newFace() { return _faces.insert(_faces.end(), Face());}
 
+		EdgeIter flipEdge(EdgeIter e); // helper function
+		VertexIter splitEdge(EdgeIter e); // helper function 
 	private:
 		list<Face> _faces;
 		list<Edge> _edges;
